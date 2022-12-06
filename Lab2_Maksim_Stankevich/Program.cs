647657e10e898440903c25d917a430a7cf9d0803
Maksim Stankevich <alvislice@gmail.com>
2022-12-06 22:09:53 +0300

    exception

--- Lab2_Maksim_Stankevich/Program.cs
+++ Lab2_Maksim_Stankevich/Program.cs
@@ -6,36 +6,51 @@ namespace Lab2_Maksim_Stankevich
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input day nubmer");
-            int number = int.Parse(Console.ReadLine());
-            switch(number)
+            while (true)
             {
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday");
-                    break;
-                case 6:
-                    Console.WriteLine("Saturday");
-                    break;
-                case 7:
-                    Console.WriteLine("Sunday");
-                    break;
-                default:
+                int number;
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("Input day nubmer");
+                    number = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
                     Console.WriteLine("Invalid input data");
-                    break;
+                    Console.ReadKey();
+                    continue;
+                }
+
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("Monday");
+                        break;
+                    case 2:
+                        Console.WriteLine("Tuesday");
+                        break;
+                    case 3:
+                        Console.WriteLine("Wednesday");
+                        break;
+                    case 4:
+                        Console.WriteLine("Thursday");
+                        break;
+                    case 5:
+                        Console.WriteLine("Friday");
+                        break;
+                    case 6:
+                        Console.WriteLine("Saturday");
+                        break;
+                    case 7:
+                        Console.WriteLine("Sunday");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input data");
+                        break;
+                }
+                Console.ReadKey();
             }
-            Console.ReadKey();
         }
     }
 }
